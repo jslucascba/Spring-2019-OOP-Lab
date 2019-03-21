49440c411f138b503851239adddbc75a72c9c8f4
Jeff Lucas <jslucas1@msn.com>
2019-03-21 16:44:54 -0500

    Danish changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,49 +4,53 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         Scanner keyboard = new Scanner(System.in);
         //Variable to accept user input
         int station;
 
+
         //Declare and instantiate a television object
         Television bigScreen = new Television("Toshiba", 55);
 
         //Turn the power on
-        bigScreen.Power();
+        bigScreen.power();
 
         //Display the state of the television
         System.out.println(bigScreen.toString());
 
         //Prompt the user for input and store it in the station variable
-        System.out.println("What channel do you want? ");
+        System.out.print("What channel do you want? ");
         station = keyboard.nextInt();
 
         //Change the channel on the television
-        bigScreen.SetChannel(station);
+        bigScreen.setChannel(station);
 
         //Increase the volume of the television
-        bigScreen.IncreaseVolume();
+        bigScreen.increaseVolume();
 
         //Display the current channel and volume
-        System.out.println("Channel: " + bigScreen.GetChannel() + " Volume: " + bigScreen.GetVolume());
+        System.out.println("Channel: " + bigScreen.getChannel() + " Volume: " + bigScreen.getVolume());
 
         System.out.println("Too loud!  Lowering the volume.");
 
         //Decrease the volume
         for(int x=0; x<6; x++)
         {
-            bigScreen.DecreaseVolume();
+            bigScreen.decreaseVolume();
         }
 
         //Display the current channel and volume
-        System.out.println("Channel: " + bigScreen.GetChannel() + " Volume: " + bigScreen.GetVolume());
+        System.out.println("Channel: " + bigScreen.getChannel() + " Volume: " + bigScreen.getVolume());
 
         //create blank line
         System.out.println();
 
+
+
         //HERE IS WHERE YOU DO TASK 5
 
-       }
+
+    }
 }
